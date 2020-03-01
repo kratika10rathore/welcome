9f9f30c3783c15ce1d8aa6d3566ffbba22f03afa
kratika10rathore <kratirathore104@gmail.com>
2020-03-01 21:46:30 +0530

    Commiting jave code changes

---
+++ HelloKrats.java
@@ -0,0 +1,10 @@
+
+public class HelloKrats {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Kratika Welcome to the Devops!");
+
+	}
+
+}
